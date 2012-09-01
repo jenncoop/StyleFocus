1ab82d9649d4c8d735d7708ae4b3207071a268a1
Jenn Cooper <JENN@.(none)>
2012-08-31 22:19:37 -0700

    clean up gen.js

--- stylefocus/gen.js
+++ stylefocus/gen.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
 
-	//$('.styleme').styleFocus(); //init
 	$('*:not(html, body)').styleFocus(); //init
 	$('*').styleFocus('destroy');
-    //var style = $('.styleme').styleFocus('show');//, '.other'); 
-	//alert(style);
 	
 });
