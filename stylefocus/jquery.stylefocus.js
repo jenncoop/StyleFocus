3d5f487fde08346e3890a7966091eb032a8141e0
Jenn Cooper <JENN@.(none)>
2012-09-04 12:12:36 -0700

    code clean-up

--- stylefocus/jquery.stylefocus.js
+++ stylefocus/jquery.stylefocus.js
@@ -1,35 +1,34 @@
-// keep all your code in a closure
 (function($)
 {
 	
 	var methods = {
 		init : function( options ) {
-			//create on/off checkbox
 			$('<div id="onOffBoxDiv"><form id="onOffForm"><input id="onOffBox" type="checkbox" name="onOffBox" checked="yes" />StyleFocus ON</form></div>').prependTo('body');
 						
 			$('#onOffBoxDiv').bind('mouseenter.styleFocus', methods.showOF);
-			$('#onOffBoxDiv').bind('mouseleave.styleFocus', methods.hideOF);
-						
+			$('#onOffBoxDiv').bind('mouseleave.styleFocus', methods.hideOF);				
 			
-				return $(this).each(function(){	
-					$(this).bind('mouseenter.styleFocus', methods.show);
-					$(this).bind('mouseleave.styleFocus', methods.hide);
-					$(this).bind('mousemove.styleFocus', methods.changePosition);										
-				});	
+			return $(this).each(function(){	
+				$(this).bind('mouseenter.styleFocus', methods.show);
+				$(this).bind('mouseleave.styleFocus', methods.hide);
+				$(this).bind('mousemove.styleFocus', methods.changePosition);										
+			});	
 			
 			
 		},
+		
 		showOF : function(){		
 			
 			$('#onOffBoxDiv').animate({top: "-1px"});
 									 
 		},
+		
 		hideOF : function(){
 		
 			$('#onOffBoxDiv').animate({top: "-20px"});
 											 
 		},
-		
+				
 		turnOnOff : function(){
 			if($('#onOffBoxDiv').attr('checked') == false){
 				$("#onOffBoxDiv").prop("checked", true);
@@ -39,6 +38,7 @@
 
 			}			
 		},
+		
 		destroy : function( ) {
 
 		   return this.each(function(){
@@ -46,72 +46,74 @@
 		   });
 	
 		},
-		cssToJs : function(css) { 	
+		
+		getCSS : function(css) { 	
 				 
-					var style = {};
-					if(!css) return style;
-					if(css instanceof CSSStyleDeclaration) {
-						for(var i in css) {
-							if((css[i]).toLowerCase) {
-								style[(css[i]).toLowerCase()] = (css[css[i]]);
-							}
-						}
-					} else if(typeof css == "string") {
-						css = css.split("; ");          
-						for (var i in css) {
-							var l = css[i].split(": ");
-							style[l[0].toLowerCase()] = (l[1]);
-						};
+			var style = {};
+			if(!css) return style;
+			if(css instanceof CSSStyleDeclaration) {
+				for(var i in css) {
+					if((css[i]).toLowerCase) {
+						style[(css[i]).toLowerCase()] = (css[css[i]]);
 					}
-					return style;
+				}
+			} else if(typeof css == "string") {
+				css = css.split("; ");          
+				for (var i in css) {
+					var line = css[i].split(": ");
+					style[line[0].toLowerCase()] = (line[1]);
+				};
+			}
+			return style;
 				
 		},
+		
 		show : function(event){
 
 			if($('#onOffBox').is(':checked')){ 
-					var $this = $(this);
-					//if(typeof element != 'undefined')
-//						{$this = element;}
+			
+				var $this = $(this);
+				
+				var sheets = document.styleSheets, styleText, formattedStyle;
+				
+				for(var i=0; i<sheets.length; i++) {
+					var rules = sheets[i].rules || sheets[i].cssRules;					
 					
-					var sheets = document.styleSheets, o = {}, styleText, formattedStyle;
+					var j = 0;
 					
-					for(var i=0; i<sheets.length;i++) {
-						var rules = sheets[i].rules || sheets[i].cssRules;					
-						
-						var j = 0;
-						
-						for(var j=0; j<rules.length;j++) {
-							if($this.is(rules[j].selectorText)) {
-								
-								if(sheets[i].href == null){
-									styleText = styleText + '<strong>Inline:</strong><br />';
-								}
-								else{
-									styleText = styleText + '<strong>External:</strong><br />';
-								}
-								
-								styleText = styleText + rules[j].cssText+'<br />';								
+					for(var j=0; j<rules.length; j++) {
+						if($this.is(rules[j].selectorText)) {
+							
+							if(sheets[i].href == null){
+								styleText = styleText + '<strong>Inline:</strong><br />';
 							}
+							else{
+								styleText = styleText + '<strong>External:</strong><br />';
+							}
+							
+							styleText = styleText + rules[j].cssText+'<br />';								
 						}
-						
-					}					
-					formattedStyle = styleText.replace('undefined','');
-					formattedStyle = formattedStyle.replace(/{/g,'{<br />');
-					formattedStyle = formattedStyle.replace(/}/g,'{<br />');
-					formattedStyle = formattedStyle.replace(/;/g,';<br />');
-  					
-					var children = $(this).children();	
-
-					if($(children).hasClass('highlight') == false){
-						$('div.styleBox').remove();
-						$('<div class="styleBox">'+ formattedStyle +'</div>').appendTo('body');
-						$(this).addClass('highlight');
 					}
+					
+				}					
+				formattedStyle = styleText.replace('undefined','');
+				formattedStyle = formattedStyle.replace(/{/g,'{<br />');
+				formattedStyle = formattedStyle.replace(/}/g,'{<br />');
+				formattedStyle = formattedStyle.replace(/;/g,';<br />');
+				
+				var children = $(this).children();	
+
+				if($(children).hasClass('highlight') == false){
+					$('div.styleBox').remove();
+					$('<div class="styleBox">'+ formattedStyle +'</div>').appendTo('body');
+					$(this).addClass('highlight');
+				}
 				
 
 			}
 	
 		},
+		
 		changePosition : function(event){
 
 			var styleBoxX = event.pageX;
@@ -119,6 +121,7 @@
 			$('.styleBox').css({'position' : 'relative', 'top' : styleBoxY, 'left' : styleBoxX});
 
 		},
+		
 		hide : function(){
 			$(this).removeClass('highlight');
 			$('div.styleBox').remove();
